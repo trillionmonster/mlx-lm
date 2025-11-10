ba2cf3c0ee72e6a8f927009c6ffe8db913decb49
n8programs <43304488+N8python@users.noreply.github.com>
2025-11-10 17:16:25 -0500

    Fix Byte Decoder Lookup for Esoteric Single-Characters  (#600)
    * tokenizer single-character fix
    
    * Update mlx_lm/tokenizer_utils.py
    
    * Update mlx_lm/tokenizer_utils.py
    
    ---------
    
    Co-authored-by: N8 <n8@n8programs.com>
    Co-authored-by: Awni Hannun <awni.hannun@gmail.com>

--- mlx_lm/tokenizer_utils.py
+++ mlx_lm/tokenizer_utils.py
@@ -210,7 +210,7 @@ class BPEStreamingDetokenizer(StreamingDetokenizer):
         # For multi-byte utf-8 wait until they are complete
         # For single spaces wait until the next token to clean it if needed
         if not text.endswith("\ufffd") and not (
-            len(v) == 1 and self._byte_decoder[v[0]] == 32
+            len(v) == 1 and self._byte_decoder.get(v[0]) == 32
         ):
             self.text += self._maybe_trim_space(text)
             self._unflushed = ""
